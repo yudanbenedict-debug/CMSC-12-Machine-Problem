5e6ebeb07da93baccd648de578c5b0abc6fc439b
codillojunewel <codillojunewel@gmail.com>
2026-04-30 22:36:44 -0700

    Platform update

--- javaBin/GamePlatform/Platform.java
+++ javaBin/GamePlatform/Platform.java
@@ -1,39 +1,33 @@
 package GamePlatform;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.util.Random;
-import java.awt.Color;
 
 public class Platform {
     protected float plat_width;
     protected float plat_height;
     protected float pos_x, pos_y;
     protected PlatformType plat_type; //the plat_type can have added types eventually as we go through making the game.
-    protected int valueX;
-    protected Random rand;
-    protected int valueY;
-    protected int[] choices = {230, 290, 350};
+    protected Graphics pl_graphics;
     private int collisionOffsetX;
     private int collisionOffsetY;
     private int collisionWidthAdjust;
     private int collisionHeightAdjust;
 
 
-    public Platform(float plat_width, float plat_height, float pos_x, float pos_y, PlatformType plat_type){
-        this.plat_width = plat_width;
-        this.plat_height = plat_height;
+    public Platform( float pos_x, float pos_y, PlatformType plat_type, Graphics pl_graphics){
+        this.plat_width = 32;
+        this.plat_height = 32;
         this.pos_x = pos_x;
         this.pos_y = pos_y;
         this.plat_type = plat_type;
-        this.rand = new Random();
-        valueX = rand.nextInt(17) * 30;
-        valueY = choices[rand.nextInt(choices.length)];
+        this.pl_graphics = pl_graphics;
         this.collisionOffsetX = 0;
         this.collisionOffsetY = 0;
         this.collisionWidthAdjust = 0;
         this.collisionHeightAdjust = 0;
-
+        
         
         
         
@@ -43,24 +37,24 @@ public class Platform {
      * @param pl_graphics
      * @param cameraX
      */
-    public final void createPlatforms(Graphics pl_graphics, int cameraX /*camera position, so we can follow */){
-        switch(plat_type){
-            case METAL : 
-                pl_graphics.setColor(new Color(90, 90, 90));
-                pl_graphics.fillRect((int) pos_x - cameraX, (int) pos_y, (int) plat_width, (int) plat_height);
-                break;
-            case WOOD :
-                pl_graphics.setColor(new Color(130, 84, 45));
-                pl_graphics.fillRect((int) pos_x - cameraX, (int) pos_y, (int) plat_width, (int) plat_height);
-                break;
-            case SAND :
-                pl_graphics.setColor(new Color(219, 183, 115));
-                pl_graphics.fillRect((int) pos_x - cameraX, (int) pos_y, (int) plat_width, (int) plat_height);
-                break;
-            default:
-                pl_graphics.setColor(Color.GRAY);
-                pl_graphics.fillRect((int) pos_x - cameraX, (int) pos_y, (int) plat_width, (int) plat_height);
-                break;
+    public void createPlatforms(int quantity, int cameraX) {
+
+        for(int i = 0; i < quantity; i++) {
+
+            int drawX = (int) this.pos_x + (i * 32) - cameraX;
+            int drawY = (int) this.pos_y;
+
+            switch(plat_type) {
+                case METAL -> pl_graphics.setColor(new Color(90, 90, 90));
+
+                case WOOD -> pl_graphics.setColor(new Color(130, 84, 45));
+
+                case SAND -> pl_graphics.setColor(new Color(219, 183, 115));
+
+                default -> pl_graphics.setColor(Color.GRAY);
+            }
+
+            pl_graphics.fillRect(drawX, drawY, 32, 32);
         }
     }
 
--- javaBin/GamePlatform/PlatformType.java
+++ javaBin/GamePlatform/PlatformType.java
@@ -3,7 +3,6 @@ package GamePlatform;
 public enum PlatformType {
     METAL(true),
     WOOD(true),
-    
     SAND(true);
     private boolean Solid;
 
